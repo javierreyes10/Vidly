155f3026ffa1a2861dc92051f8934c9776dd7f08
Javier Reyes <javier.reyes10@outlook.com>
2020-06-07 18:14:58 -0600

    Adding Cusomter and movies flow

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movieViewService = new MovieViewModel();
+            movieViewService.Initialize();
+
+            return View(movieViewService);
+        }
+    }
+}
---
+++ Vidly/ViewModels/MovieViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+using Vidly.Services;
+
+namespace Vidly.ViewModels
+{
+    public class MovieViewModel
+    {
+        private readonly IMovieService _movieService;
+        public List<Movie> Movies { get; set; }
+
+        public MovieViewModel()
+        {
+            _movieService = new MovieService();
+        }
+        public void Initialize()
+        {
+            Movies = _movieService.Movies();
+        }
+    }
+}
